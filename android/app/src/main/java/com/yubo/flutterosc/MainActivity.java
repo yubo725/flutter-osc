91a4534e700a7340fa0d03180778ce0660b89a45
yubo5 <yubo5@xiaomi.com>
2019-01-09 14:43:53 +0800

    handle android statusbar

--- android/app/src/main/java/com/yubo/flutterosc/MainActivity.java
+++ android/app/src/main/java/com/yubo/flutterosc/MainActivity.java
@@ -1,6 +1,8 @@
 package com.yubo.flutterosc;
 
+import android.os.Build;
 import android.os.Bundle;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -8,6 +10,9 @@ public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      getWindow().setStatusBarColor(0);
+    }
     GeneratedPluginRegistrant.registerWith(this);
   }
 }
